d8a4f0e829dd3d0ddd403ca6aeb37eb2a1f256b9
Yuet Chan <yuetchany@ufl.edu>
2019-11-04 02:38:25 -0500

    app-component, app-routing.module and app.module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { FirstLoginPageComponent } from './first-login-page/first-login-page.component';
+import { AWSFaceDetectTestComponent } from "./awsface-detect-test/awsface-detect-test.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path : "user/register", component : RegisterPageComponent},
+  {path : "test", component : AWSFaceDetectTestComponent},
+  {path : "user/login", component : LoginPageComponent},
+  {path : "user/emojiVerify", component : FirstLoginPageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { AWSFaceDetectTestComponent } from './awsface-detect-test/awsface-detect-test.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { FirstLoginPageComponent } from './first-login-page/first-login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    AWSFaceDetectTestComponent,
+    FirstLoginPageComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
